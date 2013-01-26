683cb36fb895d91225b428de1480c11b60b7642d
Ian Wetherbee <ian.wetherbee@gmail.com>
2013-01-26 14:22:30 -0600

    index barebones

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -4,9 +4,64 @@
 
 
 // MODELS
+var Light = ModelWS.extend({
+  defaults: function() {
+    return {
+      on: false,
+      location: "none",
+      name: "unnamed"
+    }
+  }
+});
 
 // COLLECTIONS
+var LightList = CollectionWS.extend({
+  url: "/light",
+
+  model: Light,
+
+  // sort lights by name
+  comparator: function(event) {
+    return event.get("name");
+  }
+});
+
+var Lights = new LightList();
 
 // VIEWS
+var LightListView = Backbone.View.extend({
+  tagName: "div",
+  events: {
+    "click" : "toggle"
+  },
+  template: _.template($('lightlistview-template').html()),
+  render: function() {
+    var data = this.model.toJSON();
+    this.$el.html(this.template(data));
+    return this;
+  },
+  toggle: function(event) {
+    this.model.set("on") = !this.model.get("on");
+  }
+});
 
 // ROUTER (CONTROLLER)
+var Router = Backbone.Router.extend({
+  routes: {
+    "*path": "default"
+    "lights": "lightList"
+  }
+});
+
+var AppRouter = new Router();
+AppRouter.on("route:default", function(path)) {
+  alert(path);
+}
+
+AppRouter.on("route:lightList", function() {
+
+});
+
+$(function() {
+  Backbone.history.start();
+});
