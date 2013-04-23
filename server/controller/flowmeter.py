b7ba85222ce7dca0b03a5d282c47c863b25d7fc5
mrgzg1 <gandhi.r.manan@gmail.com>
2013-04-23 17:53:31 -0500

    Commiting some changes to flowmeter, but it's not done yet

--- server/controller/flowmeter.py
+++ server/controller/flowmeter.py
@@ -1,6 +1,9 @@
 from datetime import datetime,timedelta
 
+
+
 class FlowMeterDS(object):
+'''This class is meant to be used as storage and calculation of flow for each flowmeter sensor'''
 	def __init__(self, time_period):
 		'''initialize the flowmeter datastore with the timeperiod'''
 		self.history_store = [] # basic list that we store the value in
@@ -10,10 +13,12 @@ class FlowMeterDS(object):
 	def append(self, value):
 		if not type(value) == type(1):
 			return False
-		temp_dict = {'val':value,'time_stamp'=datetime.now()}
+		temp_dict = {'val':value,'time_stamp':datetime.now()}
 		self.history_store.append(temp_dict)
 
 	def calculate_flow(self):
+		'''looks in the history store to calculate the flow flow_rate
+		it uses the last time_period data to calculate the flow_rate'''
 		filtered_vals = []
 		# remove all the values that are older then the time period
 		total_gals = 0
@@ -21,8 +26,9 @@ class FlowMeterDS(object):
 			if abs(datetime.now()-each['time_stamp']) < self.time_period:
 				filtered_vals.append(each)
 				total_gals += each['val']
+
 		#store this in the history store
 		self.history_store = filtered_vals
-		mins = 
-		flow_rate = float(total_gals) / float()
-		return 
+		#calculate the flow rate
+		flow_rate = float(total_gals) / self.time_period_min
+		return flow_rate
