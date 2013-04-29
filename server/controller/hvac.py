2a40217b47250ef849c723abcb7780be6ba779b6
mrgzg1 <gandhi.r.manan@gmail.com>
2013-04-28 20:59:31 -0500

    Changes made to HVAC model

--- server/controller/hvac.py
+++ server/controller/hvac.py
@@ -22,53 +22,51 @@ class HvacController(BackboneCollection):
 
     def parse_config(self, msg):
         for each in msg:
-            if each['UUID'] == hvac_uuids["hvac_state"]["uuid_cooling"]:
-                hvac_uuids["hvac_state"]["n_cool"] = each["n"]
-            elif each['UUID'] == hvac_uuids["hvac_state"]["uuid_heating"]:
-                hvac_uuids["hvac_state"]["n_heat"] = each["n"]
-            elif each['UUID'] == hvac_uuids["tar_temp"]["uuid"]:
-                hvac_uuids["tar_temp"]["n"] = each["n"]
+            if each['UUID'] == hvac_uuids["hvac_state_uuid"]["cooling"]:
+                hvac_uuids["hvac_state_uuid"]["n_cool"] = each["n"]
+            elif each['UUID'] == hvac_uuids["hvac_state_uuid"]["heating"]:
+                hvac_uuids["hvac_state_uuid"]["n_heat"] = each["n"]
+            elif each['UUID'] == hvac_uuids["tar_temp_uuid"]:
+                hvac_uuids["tar_temp_n"] = each["n"]
 
     def parse_state(self, msg):
         change = False
         for each in msg:
-            if hvac_uuids["hvac_state"]["n_cool"] == each["n"]:
+            if hvac_uuids["hvac_state_uuid"]["n_cool"] == each["n"]:
                 if str(each['v']) == "1":
-                    hvac_uuids["hvac_state"]["cooling"] = True
-                    hvac_uuids["hvac_state"]["val"] = "Cooling"
+                    hvac_uuids["hvac_state_uuid"]["cooling"] = True
+                    hvac_uuids["hvac_state"] = "Cooling"
                     change = True
                 else:
-                    hvac_uuids["hvac_state"]["cooling"] = False
+                    hvac_uuids["hvac_state_uuid"]["cooling"] = False
                     change = True
-            elif hvac_uuids["hvac_state"]["n_heat"] == each["n"]:
+            elif hvac_uuids["hvac_state_uuid"]["n_heat"] == each["n"]:
                 if str(each['v']) == "1":
-                    hvac_uuids["hvac_state"]["val"] = "Heating"
-                    hvac_uuids["hvac_state"]["heating"] = True
+                    hvac_uuids["hvac_state"] = "Heating"
+                    hvac_uuids["hvac_state_uuid"]["heating"] = True
                     change = True
                 else:
-                    hvac_uuids["hvac_state"]["heating"] = False
+                    hvac_uuids["hvac_state_uuid"]["heating"] = False
                     change = True
-            elif hvac_uuids["tar_temp"]["n"] == each["n"]:
-                    change = True;
-                    hvac_uuids["tar_temp"]["val"]= each["v"]
+            elif hvac_uuids["tar_temp_n"] == each["n"]:
+                    hvac_uuids["tar_temp"]= each["v"]
 
         if change:
-        #set the hvac_state to idle if both are doing nothing
-            if hvac_uuids["hvac_state"].has_key("heating") and hvac_uuids["hvac_state"].has_key("cooling"):
-                if not hvac_uuids["hvac_state"]["heating"] and not hvac_uuids["hvac_state"]["cooling"]:
-                    hvac_uuids["hvac_state"]["val"] = "Off"
+        #set the hvac_state to idle if both  are doing nothing
+            if hvac_uuids["hvac_state_uuid"].has_key("heating") and hvac_uuids["hvac_state_uuid"].has_key("cooling"):
+                if not hvac_uuids["hvac_state_uuid"]["heating"] and not hvac_uuids["hvac_state_uuid"]["cooling"]:
+                    hvac_uuids["hvac_state"] = "Off"
             self.update(hvac_uuids)
-            print "HVAC::State:",hvac_uuids['hvac_state']['val']
+            print "HVAC::State:",hvac_uuids['hvac_state']
 
 
     def on_temp_change(self, avg_temp):
         """relay the avg temp value to the loxone server"""
-        if hvac_uuids["avg_temp"].has_key("val"):
-            if hvac_uuids["avg_temp"]["val"] == avg_temp:
-                #incase the avg temp hasn't changed, don't worry about telling everyplace about this
-                return False
-        hvac_uuids["avg_temp"]["val"] = avg_temp
-        sock_str = "jdev/sps/io/"+hvac_uuids["avg_temp"]["uuid"]+"/"+str(int(avg_temp))
+        if hvac_uuids["avg_temp"] == avg_temp:
+            #incase the avg temp hasn't changed, don't worry about telling everyplace about this
+            return False
+        hvac_uuids["avg_temp"] = avg_temp
+        sock_str = "jdev/sps/io/"+hvac_uuids["avg_temp_uuid"]+"/"+str(int(avg_temp))
         print "HVAC:: Avg Temperature:", avg_temp
         #on change of average temp make sure to relay the change to both the loxone and the client
         self.sock.send_message(sock_str)
@@ -76,10 +74,10 @@ class HvacController(BackboneCollection):
 
     def do_save(self, data):
         """assuming the same model as the hvac_dict is sent back to the server"""
-        if hvac_uuids["tar_temp"]["val"] == data["tar_temp"]["val"]:
+        if hvac_uuids["tar_temp"] == data["tar_temp"]:
             #if nothing changes then just return
             return
-        hvac_uuids["tar_temp"]["val"] = data["tar_temp"]["val"]
-        sock_str = "jdev/sps/io/"+hvac_uuids["tar_temp"]["uuid"]+"/"+str(int(hvac_uuids["tar_temp"]["val"]))
-        print "HVAC:: setting temperature:", data['tar_temp']['val']
+        hvac_uuids["tar_temp"] = data["tar_temp"]
+        sock_str = "jdev/sps/io/"+hvac_uuids["tar_temp_uuid"]["uuid"]+"/"+str(int(hvac_uuids["tar_temp"]))
+        print "HVAC:: setting temperature:", data['tar_temp']
         self.sock.send_message(sock_str)
--- server/controller/hvac_dict.py
+++ server/controller/hvac_dict.py
@@ -1,16 +1,12 @@
 hvac_uuids = {
-	"avg_temp":{
-		"val": 22,
-		"uuid":"ec016bfe-a16a-11e2-82d0eb875f37995e",
+	"avg_temp":22,
+	"avg_temp_uuid":"ec016bfe-a16a-11e2-82d0eb875f37995e",
+	"tar_temp":22,
+	"tar_temp_uuid":"58094eba-a164-11e2-9ebdeb875f37995e",
+	"hvac_state_uuid":{
+		"cooling":"a8986b16-a174-11e2-9ee3eb875f37995e",
+		"heating":"9e6dbafa-a174-11e2-9e45eb875f37995e",
 	},
-	"tar_temp":{
-		"uuid":"58094eba-a164-11e2-9ebdeb875f37995e",
-		"val": 22,
-	},
-	"hvac_state":{
-		"uuid_cooling":"a8986b16-a174-11e2-9ee3eb875f37995e",
-		"uuid_heating":"9e6dbafa-a174-11e2-9e45eb875f37995e",
-		"val":"Off",	#Off, Cooling, Heating
-	},
+	"hvac_state":"Off",#Off, Cooling, Heating
 	"id":"hvac_object"
 }
